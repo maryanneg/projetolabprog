21abaee5142d50e65dbe9c490515177a8ef46ed1
Maryanne Gomes <maryannegomes99@gmail.com>
2020-10-15 23:03:15 -0300

    integracao com leitura do csv

--- telaupload (2).py
+++ telaupload (2).py
@@ -1,5 +1,5 @@
 import PySimpleGUI as sg
-
+import csv
 sg.theme('Green')
 layout = [  [sg.Text('Tela de Upload', font=('arial',15),justification='center', size=(50, 1))],
             [sg.Text('Escolha o Arquivo',size=(15,5)), sg.Input('',key='upload'),sg.FileBrowse()],
@@ -17,4 +17,7 @@ while True:
     if  eventos == 'Enviar':
         if valores['upload']:
             print('Envio Concluido', valores['upload'])
-
+            with open(valores['upload']) as arquivo_csv:
+                leitor = csv.reader(arquivo_csv, delimiter=',')
+                for coluna in leitor:
+                    print(coluna)
