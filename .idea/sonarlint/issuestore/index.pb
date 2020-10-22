a63fc1bb27da1a7a947be54e7ca3e50ee6accfda
Maryanne Gomes <maryannegomes99@gmail.com>
2020-10-22 15:07:52 -0300

    rebase

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+A
+telaupload (2).py,1\0\104cc968ee3d9721559ef9c2d7efea85e78ca753
--- src/telaupload (2).py
+++ src/telaupload (2).py
@@ -1,27 +1,29 @@
 import PySimpleGUI as sg
 import csv
-sg.theme('Green')
-layout = [  [sg.Text('Tela de Upload', font=('arial',15),justification='center', size=(50, 1))],
-            [sg.Text('Escolha o Arquivo',size=(15,0)), sg.Input('',key='upload'),sg.FileBrowse()],
-            [sg.Button('Enviar'), sg.Button('Cancelar')],
-            [sg.Output(size=(71, 10))]]
+def tela_upload():
+    sg.theme('Green')
+    layout = [  [sg.Text('Tela de Upload', font=('arial',15),justification='center', size=(50, 1))],
+            [sg.Text('Escolha o Arquivo',size=(15,5)), sg.Input('',key='upload'),sg.FileBrowse()],
+            [sg.Button('Enviar'), sg.Button('Cancelar')] ]
+    return sg.Window('Tela de Upload', layout = layout, finalize=True)
+def tela_dados():
+    layout = [[sg.Text('Tela de Dados', font=('arial', 15), justification='center', size=(50, 1))]
 
 
-tela = sg.Window('Tela de Upload', layout)
-
+              ]
+    return sg.Window('Dados do Arquivo', layout = layout, finalize= True)
+janela1, janela2 = tela_upload(), None
 while True:
-    eventos, valores = tela.read()
-    if eventos == sg.WINDOW_CLOSED:
+    janela, eventos, valores = sg.read_all_windows()
+    if janela == janela1 and eventos == sg.WINDOW_CLOSED and eventos == 'Cancelar':
         break
-    elif eventos == 'Cancelar':
-        break
-    if  eventos == 'Enviar':
+    if  janela ==janela1 and eventos == 'Enviar':
 
         if valores['upload']:
-            print('Envio Concluido', valores['upload'])
+            print('Envio Concluido')
+            janela2 = tela_dados()
+            janela1.hide()
             with open(valores['upload']) as arquivo_csv:
                 leitor = csv.reader(arquivo_csv, delimiter=',')
                 for coluna in leitor:
-                    print(coluna)
-
-                    
+                        print(coluna)
